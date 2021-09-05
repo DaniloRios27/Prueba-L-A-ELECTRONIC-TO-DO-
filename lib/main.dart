a2c80f56b320f8d0cfa0968c323150785b5ab63b
Danilo Rios <danilorios22@gmail.com>
2021-09-05 18:36:03 -0500

    Boceto Página Principal

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+import 'package:to_do_app/screens/toDoHomePage.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: ToDoHomePage(),
+  ));
+}
---
+++ lib/screens/toDoHomePage.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class ToDoHomePage extends StatefulWidget {
+@override
+  _StateToDoHomePage createState() => _StateToDoHomePage();
+}
+
+class _StateToDoHomePage extends State<ToDoHomePage> {
+@override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+
+      ),
+      body: Container(
+
+      ),
+    );
+  }
+}
